f453ae9d4fba9e1a9712ff8f2703fb1c6da29a04
MarcelloNunes <marcellongois@gmail.com>
2023-07-22 15:20:16 -0300

    feat: Desenvolvimento dos serviços de produtos

---
+++ src/main/java/br/com/cotiinformatica/entities/Produto.java
@@ -0,0 +1,37 @@
+package br.com.cotiinformatica.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "produto")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idProduto")
+	private Integer idProduto;
+	
+	@Column(name = "nome", length = 150, nullable = false)
+	private String nome;
+	
+	@Column(name = "preco", nullable = false)
+	private Double preco;
+	
+	@Column(name = "quantidade", nullable = false)
+	private Integer quantidade;
+
+}
